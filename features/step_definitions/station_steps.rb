ca42d0c12cb2f62afddccb9b29277b79052ff63f
Peter <peterhsaxton@gmail.com>
2014-04-30 19:25:09 +0100

    Fixed test for JSON content

--- features/step_definitions/station_steps.rb
+++ features/step_definitions/station_steps.rb
@@ -8,5 +8,6 @@ end
 
 Then(/^I should see the station$/) do
   expect(last_response.status).to eq 200
-  expect(last_response.body).to eq(11)
+  data = JSON.parse(last_response.body, symbolize_names: true)
+  expect(data.first).to include(:name)
 end
