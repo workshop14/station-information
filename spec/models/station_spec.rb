76d2a0cfce85e81b3bd4f7cbab17ea055933fa9f
Peter <peterhsaxton@gmail.com>
2014-05-01 09:47:36 +0100

    Validates prescence of name

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -5,51 +5,18 @@ FactoryGirl.define do
     name 'Wapping'
     address 'Wapping Station, London Underground Ltd., Wapping High St, London, E1 9NQ'
     postcode 'E1 9NQ'
-    longitude -0.055846250896647850
+    longitude(-0.055846250896647850)
     latitude 51.504268283481814000
   end
 end
 
 describe Station do
   context 'Required details' do
-    subject(:station) { build(:station) }
-    its(:name) { should eq('Wapping') }
-  end
-  it 'should create a valid station' do
-    station = Station.new name: 'Wapping'
-    expect(station).to be_valid
-  end
-
-  it 'should work with factory girl' do
-    FactoryGirl.create(:station)
-    station = Station.first
-    expect(station.name).to eq('Wapping')
-  end
-
-  it 'should work with factory girl name overwrite' do
-    FactoryGirl.create(:station, name: 'Kennington')
-    station = Station.first
-    expect(station.name).to eq('Kennington')
-    expect(Station.all.length).to eq(1)
-  end
-
-  it 'should be possible to remove FactoryGirl namespace' do
-    bank = build(:station, name: 'bank')
-    expect(Station.all.length).to eq(0)
-    expect(bank).to be_valid
-    expect(bank.save).to be_true
-    expect(Station.all.length).to eq(1)
-  end
-
-  describe 'special' do
-    subject(:station) { create(:station) }
-    its(:name) { should == 'Wapping' }
-    it 'should be valid' do
-      expect(subject).to be_valid
+    it 'should be invalid without a name' do
+      station = build(:station, name: nil)
+      expect(station).not_to be_valid
+      expect(station.errors[:name]).not_to be_empty
     end
-    it 'should be valid' do
-      expect(station).to be_valid
-    end
-    it { should be_valid }
   end
+
 end
