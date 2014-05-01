d7dccec33fd085ecc52edcaaf615f6f58c8bbb20
Peter <peterhsaxton@gmail.com>
2014-05-01 08:31:11 +0100

    Added covereage reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
